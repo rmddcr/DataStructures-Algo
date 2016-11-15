155f379394840689a472f60c8d19de12b8b130d9
pasindu <pasindubawantha@gmail.com>
2016-11-15 12:42:03 +0530

    llol

---
+++ Btree.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self,value):
+        self.parent = None
+        self.left = None
+        self.Right = None
+        self.data = value
+
+
+class Btree:
+    def __init__(self):
+        self.root = None
+
+    def find(self,value,current=True):
+        if current ==-1
+
+    def insert(self,value):
